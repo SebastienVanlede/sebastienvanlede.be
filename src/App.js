2c3dad523bc0dfd09126975877777c50a2a2b9c9
Sebastien Vanlede <sebastien.vanlede@student.hogent.be>
2022-04-07 17:12:49 +0200

    🚀

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
-import { ThemeContext } from './contexts/ThemeContext';
 import { Main, ProjectPage } from './pages'
 import { BackToTop } from './components'
 import ScrollToTop from './utils/ScrollToTop'
@@ -10,12 +9,6 @@ import './App.css'
 
 function App() {
 
-  const { theme } = useContext(ThemeContext);
-
-  console.log("%cDEVELOPER PORTFOLIO", `color:${theme.primary}; font-size:50px`);
-  console.log("%chttps://github.com/hhhrrrttt222111/developer-portfolio", `color:${theme.tertiary}; font-size:20px`);
-  // console.log = console.warn = console.error = () => {};
-
   return (
     <div className="app">
       <Router>
@@ -23,7 +16,6 @@ function App() {
         <Switch>
           <Route path="/" exact component={Main} />
           <Route path="/projects" exact component={ProjectPage} />
-
           <Redirect to="/" />
         </Switch>
       </Router>
