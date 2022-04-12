00212761b00027de19ab21d14ca58a4877f7cfdb
Sebastien Vanlede <sebastien.vanlede@student.hogent.be>
2022-04-12 14:46:54 +0200

    address change

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
     email: 'sebastien.vanlede@outlook.be',
     phone: '+32495383212',
-    address: 'Vlieguit 28, 9830 Sint-Martens-Latem, Belgium',
+    address: 'Ghent, Belgium',
 
     sheetAPI: ''
 }
